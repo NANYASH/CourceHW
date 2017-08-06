ca55be0b677d080f5a0795ad008113695b34ec96
nanyash <miss.nanya80@gmail.com>
2017-08-06 12:20:55 +0300

    Custom Arraylist (invalid remove methods)

---
+++ src/main/java/cource/task6/Main.java
@@ -0,0 +1,14 @@
+package cource.task6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new CustomList<>();
+        list.add("My name is Nastia");
+        list.remove(0);
+
+    }
+
+}
