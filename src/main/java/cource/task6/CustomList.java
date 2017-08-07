5a4638ee30d1689483564a63eb012b1818729b21
nanyash <miss.nanya80@gmail.com>
2017-08-08 00:33:31 +0300

    Custom Arraylist (invalid remove methods)

--- src/main/java/cource/task6/CustomList.java
+++ src/main/java/cource/task6/CustomList.java
@@ -6,36 +6,36 @@ import java.util.Collection;
 
 public class CustomList<E> extends ArrayList<E> {
 
-    private static final String EXEPTION_MESSAGE = "Invalid operation";
+    private static final String EXCEPTION_MESSAGE = "Invalid operation";
 
     @Override
     public E set(int index, Object element) {
-        throw new UnsupportedOperationException(EXEPTION_MESSAGE );
+        throw new UnsupportedOperationException(EXCEPTION_MESSAGE);
     }
 
     @Override
     public E remove(int index) {
-        throw new UnsupportedOperationException(EXEPTION_MESSAGE );
+        throw new UnsupportedOperationException(EXCEPTION_MESSAGE);
     }
 
     @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException(EXEPTION_MESSAGE );
+        throw new UnsupportedOperationException(EXCEPTION_MESSAGE);
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException(EXEPTION_MESSAGE );
+        throw new UnsupportedOperationException(EXCEPTION_MESSAGE);
     }
 
     @Override
     protected void removeRange(int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException(EXEPTION_MESSAGE );
+        throw new UnsupportedOperationException(EXCEPTION_MESSAGE);
     }
 
     @Override
     public boolean removeAll(Collection c) {
-        throw new UnsupportedOperationException(EXEPTION_MESSAGE );
+        throw new UnsupportedOperationException(EXCEPTION_MESSAGE);
     }
 
 }
