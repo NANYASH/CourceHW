409ba71ff99f03a4346c94bd755a71dc618e8a2a
nanyash <miss.nanya80@gmail.com>
2017-08-10 12:21:37 +0300

    Factory method pattern for "create, selected by customer, watch" task

---
+++ src/main/java/cource/task7/FactoryMethod.java
@@ -0,0 +1,59 @@
+package cource.task7;
+
+import java.util.Date;
+
+
+public class FactoryMethod {
+    public static void main(String[] args) {
+
+
+        WatchMaker watchMaker = getMakerByName("Digital");
+
+
+    }
+    public static WatchMaker getMakerByName(String maker){
+        if (maker.equals("Digital"))
+            return new DigitalWatchMaker();
+        if (maker.equals("Rome"))
+            return new RomeWatchMaker();
+
+        throw new  RuntimeException("Not found maker!"+maker);
+
+    }
+}
+
+interface Watch{
+    void showTime();
+
+}
+
+class DigitalWatch implements Watch {
+
+    public void showTime() {
+        System.out.println(new Date());
+    }
+}
+
+
+class RomeWatch implements Watch {
+    public void showTime() {
+        System.out.println("VII-XX");
+    }
+}
+
+interface WatchMaker {
+    Watch createWatch();
+}
+
+class DigitalWatchMaker implements WatchMaker {
+
+    public Watch createWatch() {
+       return new DigitalWatch();
+    }
+}
+
+class RomeWatchMaker implements WatchMaker {
+    public Watch createWatch() {
+        return new RomeWatch();
+    }
+}
