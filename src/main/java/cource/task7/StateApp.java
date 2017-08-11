eb826c442c9e2f4986274264829c3399a1cdfafe
nanyash <miss.nanya80@gmail.com>
2017-08-11 14:19:35 +0300

    State pattern for "human1" task

---
+++ src/main/java/cource/task7/StateApp.java
@@ -0,0 +1,47 @@
+package cource.task7;
+
+
+public class StateApp {
+    public static void main(String[] args) {
+        Activity context = new Activity();
+        context.setHuman("see animal");
+        context.human.introduce();
+        context.setHuman("mushroom field");
+        context.human.introduce();
+
+    }
+}
+
+//State
+interface Human{
+    void introduce();
+}
+class Hunter implements Human{
+    public void introduce() {System.out.println("I'm a hunter");}
+}
+class Fisher implements Human{
+    public void introduce() {System.out.println("I'm a fisher");}
+}
+class MushroomerPicker implements Human{
+    public void introduce() {System.out.println("I'm a mushroom picker");}
+}
+
+//Context
+class Activity {
+    Human human;
+    void setHuman(String position){
+        switch (position){
+            case "bank":
+                human = new Fisher();
+                break;
+            case "see animal":
+                human = new Hunter();
+                break;
+            case "mushroom field":
+                human = new MushroomerPicker();
+                break;
+            default:
+                System.out.println("No such activity!");
+        }
+    }
+}
